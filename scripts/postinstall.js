369042b91515e9739d1aaa7566ceb18d05315486
Kiko Beats <josefrancisco.verdu@gmail.com>
2025-10-28 12:30:39 +0100

    fix: mql query

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,22 +3,25 @@
 'use strict'
 
 const jsonFuture = require('json-future')
+
 const mql = require('@microlink/mql')
 
 const main = async () => {
   const { data } = await mql('https://spongebob.fandom.com/wiki/List_of_time_cards', {
     data: {
-      timecards: {
-        selectorAll: 'td > a.image',
-        attr: 'href',
+      images: {
+        selectorAll: 'td a > img',
+        attr: 'data-src',
         type: 'image'
       }
     }
   })
 
-  const { timecards } = data
-  const images = timecards.filter(Boolean)
-  return jsonFuture.saveAsync('data.json', images)
+  const { images } = data
+
+  const links = images.filter(Boolean).map(image => image.url.replace(/\/revision.*/, ''))
+
+  return jsonFuture.saveAsync('data.json', links)
 }
 
 main().catch(error => console.error(error) || process.exit(1))
